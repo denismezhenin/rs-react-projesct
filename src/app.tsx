a535a3b7c0846f2a0a915b7650058ce656abec6c
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2023-03-19 23:57:32 +0100

    init: start project

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+// import ReactDOM from "react-dom/client";
+// import { useState } from "react";
+
+class App extends Component {
+  render() {
+    return <h1>Hello!</h1>;
+  }
+}
+
+export default App;
